c6287efe87855f03332cea1120157567842e8afa
Oreo <9503060405d@gmail.com>
2022-10-20 23:30:31 +0800

    登录方法 demo

--- main.go
+++ main.go
@@ -1,28 +1,60 @@
 package main
 
 import (
+	"crypto/md5"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
+	"strconv"
 	"strings"
+	"time"
 )
 
 func sayhelloName(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-	fmt.Println(r.Form)
+	r.ParseForm() // 解析 url 传递的参数，对于 POST 则解析响应包的主体（request body）
+
+	// 没有调用 ParseForm 方法，下面无法获取表单的数据
+	fmt.Println(r.Form) // 输出到服务端的打印信息
 	fmt.Println("Path", r.URL.Scheme)
 	fmt.Println(r.Form["url_long"])
 	for k, v := range r.Form {
 		fmt.Println("key:", k)
 		fmt.Println("val:", strings.Join(v, ""))
 	}
-	fmt.Fprintf(w, "Hello world!")
+	fmt.Fprintf(w, "Hello world!") // 写入到 w 的是输出到客户端的信息
 }
 
 func main() {
-	http.HandleFunc("/", sayhelloName)
-	err := http.ListenAndServe(":9090", nil)
+	http.HandleFunc("/", sayhelloName)       // 设置访问的路由
+	http.HandleFunc("/login", login)         // 设置访问的路由
+	err := http.ListenAndServe(":9090", nil) // 设置监听的端口
 	if err != nil {
 		log.Fatal("ListenAndServe:", err)
+	}
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method)
+	if r.Method == "GET" {
+		timestamp := strconv.Itoa(time.Now().Nanosecond())
+		hashWr := md5.New()
+		hashWr.Write([]byte(timestamp))
+		token := fmt.Sprintf("%x", hashWr.Sum(nil))
+
+		t, _ := template.ParseFiles("login.gtpl")
+		log.Println(t.Execute(w, token))
+	} else {
+		r.ParseForm()
+		token := r.Form.Get("token")
+		if token != "" {
+
+		} else {
+
+		}
+		fmt.Println("username length:", len(r.Form["username"][0]))
+		fmt.Println("username:", template.HTMLEscapeString(r.Form.Get("username")))
+		fmt.Println("password:", template.HTMLEscapeString(r.Form.Get("password")))
+		template.HTMLEscape(w, []byte(r.Form.Get("username")))
 	}
 }
